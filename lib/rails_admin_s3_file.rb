00519e371eb89086687d4fbe1100ca847aa60736
Oscar Siniscalchi <oscar.siniscalchi@neonroots.com>
2017-05-23 18:05:31 -0300

    Move AWS config to this gem.

--- lib/rails_admin_s3_file.rb
+++ lib/rails_admin_s3_file.rb
@@ -13,15 +13,48 @@ module RailsAdmin
         class S3File < RailsAdmin::Config::Fields::Base
           RailsAdmin::Config::Fields::Types::register(self)
 
+          def initialize
+            Aws.config.update(
+              region: ENV['AWS_REGION'] || 'us-east-1',
+              credentials: Aws::Credentials
+                .new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+            )
+            super
+          end
+
           register_instance_option(:partial) do
     	      :form_s3_file
     	    end
 
+          register_instance_option(:upload_path) do
+            'uploads'
+    	    end
+
+          register_instance_option(:upload_extension) do
+            'mp4'
+    	    end
+
+          register_instance_option(:upload_acl) do
+            'public-read'
+    	    end
+
+          register_instance_option(:upload_content_type) do
+            'video'
+          end
+
+          def upload_key
+            "#{upload_path}/#{SecureRandom.uuid}.#{upload_extension}"
+          end
+
+          def s3_bucket
+            @bucket ||= Aws::S3::Resource.new.bucket(ENV['FOG_DIRECTORY'])
+          end
+
           def presigned_post
-            S3_BUCKET.presigned_post(
-              key: "#{ENV['AWS_ENV']}/videos/#{SecureRandom.uuid}.mp4",
-              acl: 'public-read',
-              content_type_starts_with: 'video',
+            @bucket.presigned_post(
+              key: upload_key,
+              acl: upload_acl,
+              content_type_starts_with: upload_content_type,
               expires: ::Time.now + 1.hour
             )
           end
--- lib/rails_admin_s3_file/version.rb
+++ lib/rails_admin_s3_file/version.rb
@@ -1,3 +1,3 @@
 module RailsAdminS3File
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 end
